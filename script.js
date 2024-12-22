3360a50979a2498997c5ad1191e3a3850816df4b
ikra002 <irina.vig04@gmail.com>
2024-12-23 00:44:24 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Страница загружена и готова к использованию!");
+    
+    const greeting = () => {
+        alert("Добро пожаловать на мой сайт о книгах!");
+    };
+
+    greeting();
+});
